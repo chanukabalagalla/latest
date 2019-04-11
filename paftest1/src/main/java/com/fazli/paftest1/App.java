a097ffe70a146693ebba05e95e1d09d47becd0ba
fazlicodes <fazlimaam@gmail.com>
2019-04-11 11:05:39 +0530

    Fazli's

---
+++ paftest1/src/main/java/com/fazli/paftest1/App.java
@@ -0,0 +1,13 @@
+package com.fazli.paftest1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
